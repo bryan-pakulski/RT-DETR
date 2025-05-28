c8208de4992a33b4fabe84fb59e5261d99686df5
Bryan <BryanPakulski@outlook.com>
2025-05-29 00:19:45 +1000

    code seperation

--- rtdetrv2_pytorch/src/solver/det_solver.py
+++ rtdetrv2_pytorch/src/solver/det_solver.py
@@ -59,25 +59,7 @@ class DetSolver(BaseSolver):
             # will cause hanging while we wait for each GPU to finish training and the first gpu to finish is calling for a sync
             # We subset the dataset into mini batches if required so each GPU has the same number of samples during training
             if (dist_utils.is_parallel(self.model) and len(args.device_batch_split) > 0):
-                _total_size = len(self.train_dataloader) * args.train_dataloader.batch_size
-                _minibatch_size = (_total_size // args.total_batch_size) * args.train_dataloader.batch_size
-
-                # Calculate the offset for our subset of the dataset based on rank position of the current gpu, this should mean that we guarantee the whole dataset is seen per each epoch
-                _entries_per_batch = _total_size // args.total_batch_size
-                _start_idx = 0
-                for idx, b in enumerate(args.device_batch_split):
-                    if idx == dist_utils.get_rank():
-                        break
-                    start_idx += b * _entries_per_batch                
-                subset =  torch.utils.data.DataLoader(torch.utils.data.Subset(self.train_dataloader.dataset, range(start_idx, _start_idx + _minibatch_size)),  
-                                batch_size=args.train_dataloader.batch_size, 
-                                num_workers=args.train_dataloader.num_workers, 
-                                collate_fn=args.train_dataloader.collate_fn,
-                                shuffle=args._train_shuffle,)
-                subset = dist_utils.warp_loader(subset, shuffle=args._train_shuffle)
-
-                gprint(f"training minibatch subset index: {start_idx} -> {_start_idx + _minibatch_size} for rank: {dist_utils.get_rank()}")
-                gprint(f"total training size: {_minibatch_size}/{_total_size} for rank: {dist_utils.get_rank()}")
+                dist_utils.subset_dataset_by_rank(args, self.train_dataloader)
             else:
                 subset = self.train_dataloader
 
@@ -191,25 +173,7 @@ class DetSolver(BaseSolver):
         # We subset the dataset into mini batches if required so each GPU has the same number of samples during training
         args = self.cfg
         if (dist_utils.is_parallel(self.model) and len(args.device_batch_split) > 0):
-            _total_size = len(self.val_dataloader) * args.val_dataloader.batch_size
-            _minibatch_size = (_total_size // args.total_batch_size) * args.val_dataloader.batch_size
-
-            # Calculate the offset for our subset of the dataset based on rank position of the current gpu, this should mean that we guarantee the whole dataset is seen per each epoch
-            _entries_per_batch = _total_size // args.total_batch_size
-            _start_idx = 0
-            for idx, b in enumerate(args.device_batch_split):
-                if idx == dist_utils.get_rank():
-                    break
-                start_idx += b * _entries_per_batch                
-            subset =  torch.utils.data.DataLoader(torch.utils.data.Subset(self.val_dataloader.dataset, range(start_idx, _start_idx + _minibatch_size)),  
-                            batch_size=args.val_dataloader.batch_size, 
-                            num_workers=args.val_dataloader.num_workers, 
-                            collate_fn=args.val_dataloader.collate_fn,
-                            shuffle=args._train_shuffle,)
-            subset = dist_utils.warp_loader(subset, shuffle=args._train_shuffle)
-
-            gprint(f"validation minibatch subset index: {start_idx} -> {_start_idx + _minibatch_size} for rank: {dist_utils.get_rank()}")
-            gprint(f"total validation size: {_minibatch_size}/{_total_size} for rank: {dist_utils.get_rank()}")
+            dist_utils.subset_dataset_by_rank(args, self.val_dataloader)
         else:
             subset = self.val_dataloader
 
